75b5ffb2524ff6418f21f308ad92c41576002379
Nowshad <nowshad6196@gmail.com>
2023-06-17 19:28:21 +0530

    Added 121. Best time to buy and sell stock 42. Trapping Rain water
    Updated
    11. Container_With_Most_Water

--- src/A_Arrays/B_Medium/_0011_Container_With_Most_Water.java
+++ src/A_Arrays/B_Medium/_0011_Container_With_Most_Water.java
@@ -2,22 +2,44 @@ package A_Arrays.B_Medium;
 
 public class _0011_Container_With_Most_Water {
 
-    class Solution {
-        public int maxArea(int[] height) {
-            int maxArea = 0;
-            int left = 0;
-            int right = height.length - 1;
-            while (left < right) {
-                int l = right - left;
-                int h = Math.min(height[left], height[right]);
-                maxArea = Math.max(maxArea, (l * h));
-                if (height[left] <= height[right]) {
-                    left++;
-                } else {
-                    right--;
-                }
+    public static void main(String[] args) {
+        // Create an instance of the Solution class
+        Solution solution = new Solution();
+
+        // Test case: height = [1, 8, 6, 2, 5, 4, 8, 3, 7]
+        int[] height = {1, 8, 6, 2, 5, 4, 8, 3, 7};
+        int maxArea = solution.maxArea(height);
+        System.out.println("Max area for height [1, 8, 6, 2, 5, 4, 8, 3, 7]: " + maxArea);
+        // Expected output: 49 (The maximum area is achieved by selecting the heights 8 and 7, with a width of 7 - 2 = 5. The area is 5 * 7 = 35)
+
+    }
+}
+
+class Solution {
+    public int maxArea(int[] height) {
+        int left = 0;
+        int right = height.length - 1;
+        int maxArea = 0;
+
+        // Continue the loop until the pointers meet or cross each other
+        while (left < right) {
+            // Calculate the width as the difference between the right and left pointers
+            int width = right - left;
+            // Calculate the height as the minimum value between the heights at the left and right pointers
+            int minHeight = Math.min(height[left], height[right]);
+
+            // Calculate the current area using width and minHeight, and update maxArea if necessary
+            maxArea = Math.max(width * minHeight, maxArea);
+
+            // Move the pointer that corresponds to the smaller height
+            if (height[left] < height[right]) {
+                left++;
+            } else {
+                right--;
             }
-            return maxArea;
         }
+
+        // Return the maximum area
+        return maxArea;
     }
 }
---
+++ src/A_Arrays/C_Hard/_0042_Trapping_Rain_Water.java
@@ -0,0 +1,56 @@
+package A_Arrays.C_Hard;
+
+public class _0042_Trapping_Rain_Water {
+    public static void main(String[] args) {
+        // Create an instance of the Solution class
+        Solution solution = new Solution();
+
+        // Test case: height = [0,1,0,2,1,0,1,3,2,1,2,1]
+        int[] height = {0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1};
+        int trappedWater = solution.trap(height);
+        System.out.println("Trapped water for height [0,1,0,2,1,0,1,3,2,1,2,1]: " + trappedWater);
+        // Expected output: 6 (The height array represents a series of bars, and the trapped water is calculated based on the bar heights. In this case, there are 6 units of water trapped between the bars.)
+    }
+}
+class Solution {
+    public int trap(int[] height) {
+        // Check if the height array is empty
+        if (height.length == 0) {
+            return 0;
+        }
+
+        // Initialize the left and right pointers
+        int left = 0;
+        int right = height.length - 1;
+
+        // Initialize the maximum height on the left and right sides
+        int maxL = height[left];
+        int maxR = height[right];
+
+        // Initialize the result variable to store the trapped water amount
+        int result = 0;
+
+        // Loop until the left and right pointers meet or cross each other
+        while (left < right) {
+            // Compare the maximum heights on the left and right sides
+            if (maxL < maxR) {
+                // Move the left pointer to the next position
+                left++;
+                // Update the maximum height on the left side if necessary
+                maxL = Math.max(maxL, height[left]);
+                // Calculate the amount of water trapped between the current position and the maximum height on the left side
+                result += maxL - height[left];
+            } else {
+                // Move the right pointer to the previous position
+                right--;
+                // Update the maximum height on the right side if necessary
+                maxR = Math.max(maxR, height[right]);
+                // Calculate the amount of water trapped between the current position and the maximum height on the right side
+                result += maxR - height[right];
+            }
+        }
+
+        // Return the total amount of trapped water
+        return result;
+    }
+}
