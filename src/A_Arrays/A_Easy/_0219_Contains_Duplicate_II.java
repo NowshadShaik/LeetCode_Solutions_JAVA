3655fa714dfe2ee54227331c864b591933cd7a3f
Nowshad <nowshad7037@gmail.com>
2023-10-02 19:13:36 +0530

    Added 88. Merge Sorted Array 35. Search Insert Position 66. Plus One 219. Contains Duplicate II

---
+++ src/A_Arrays/A_Easy/_0219_Contains_Duplicate_II.java
@@ -0,0 +1,22 @@
+package A_Arrays.A_Easy;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class _0219_Contains_Duplicate_II {
+
+    class Solution {
+        public boolean containsNearbyDuplicate(int[] nums, int k) {
+            Map<Integer, Integer> hashMap = new HashMap<>();
+            for(int i=0;i<nums.length;i++) {
+                if(hashMap.containsKey(nums[i])) {
+                    if(Math.abs(hashMap.get(nums[i]) - i) <=k) {
+                        return true;
+                    }
+                }
+                hashMap.put(nums[i],i);
+            }
+            return false;
+        }
+    }
+}
