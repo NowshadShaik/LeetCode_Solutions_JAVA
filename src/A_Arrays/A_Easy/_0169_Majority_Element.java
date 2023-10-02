a0ce6a890f3ae10c4130fbcc7d125c26a064d01e
Nowshad <nowshad7037@gmail.com>
2023-10-02 19:13:14 +0530

    Added 1822. Sign of the Product of an Array 169. Majority Element 1491. Average Salary Excluding the Minimum and Maximum Salary 228. Summary Ranges

---
+++ src/A_Arrays/A_Easy/_0169_Majority_Element.java
@@ -0,0 +1,23 @@
+package A_Arrays.A_Easy;
+
+public class _0169_Majority_Element {
+
+    class Solution {
+        public int majorityElement(int[] nums) {
+
+            int count=0, ans=0;
+
+            for(int i=0;i<nums.length;i++) {
+                if(count == 0 ) {
+                    ans = nums[i];
+                }
+                if(nums[i]==ans) {
+                    count++;
+                } else {
+                    count--;
+                }
+            }
+            return ans;
+        }
+    }
+}
