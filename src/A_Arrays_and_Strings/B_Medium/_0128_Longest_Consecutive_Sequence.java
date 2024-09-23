bda9ec6b71bbd0a05c6ccdfd6f77e4ca51340ed9
Nowshad Shaik <nowshad7037@gmail.com>
2024-09-23 21:08:12 +0530

    Add solutions: 49, 128, 271

---
+++ src/A_Arrays_and_Strings/B_Medium/_0128_Longest_Consecutive_Sequence.java
@@ -0,0 +1,30 @@
+package A_Arrays_and_Strings.B_Medium;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class _0128_Longest_Consecutive_Sequence {
+
+        public int longestConsecutive(int[] nums) {
+        if(nums.length == 0) return 0;
+        
+        int max = 1;
+        Set<Integer> set = new HashSet<>();
+        for(int n : nums) set.add(n);
+
+        for(int n : nums) {
+            int tempMax = 1, x=n;
+            if(!set.contains(x+1)) {
+                while(set.contains(x-1)) {
+                    tempMax++;
+                    x--;
+                }
+                max = Math.max(tempMax, max);
+                if(max>(nums.length/2)) {
+                    return max;
+                }
+            }
+        }
+        return max;
+    }
+}
