5d806f2ac968c0c482a6232143db324ac84b03e4
Nowshad Shaik <nowshad7037@gmail.com>
2024-09-25 18:40:49 +0530

    Add 314, 541, 557, 977

---
+++ src/B_Two_Pointers/A_Easy/_0977_Squares_of_Sorted_Array.java
@@ -0,0 +1,19 @@
+package B_Two_Pointers.A_Easy;
+
+public class _0977_Squares_of_Sorted_Array {
+
+    public int[] sortedSquares(int[] nums) {
+        int l = nums.length-1;
+        int[] ans = new int[l+1];
+        int p1 = 0, p2 = l;
+
+        while(p1<=p2) {
+            int a = Math.abs(nums[p1] * nums[p1]);
+            int b = Math.abs(nums[p2] * nums[p2]);
+            ans[l--] = Math.max(a,b);
+            if(a>b) p1++;
+            else p2--;
+        }
+        return ans;
+    }
+}
