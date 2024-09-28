a3fbd32c5c596440d28f15a587dc857cfd3cbd28
Nowshad Shaik <nowshad7037@gmail.com>
2024-09-28 18:53:30 +0530

    Add solutions: 209, 424, 643, 1004

---
+++ src/Sliding_Window/A_Easy/_0643_Maximum_Average_Subarray.java
@@ -0,0 +1,20 @@
+package Sliding_Window.A_Easy;
+
+public class _0643_Maximum_Average_Subarray {
+
+    public double findMaxAverage(int[] nums, int k) {
+        int l = 0, r = k - 1;
+        double sum = 0;
+        for (int i = 0; i < k; i++) {
+            sum += nums[i];
+        }
+        double avg = sum / k;
+        r = k;
+        while (r < nums.length) {
+            sum -= nums[l++];
+            sum += nums[r++];
+            avg = Math.max(avg, sum / k);
+        }
+        return avg;
+    }
+}
---
+++ src/Sliding_Window/B_Medium/_0209_Minimum_Size_Subarray_Sum.java
@@ -0,0 +1,21 @@
+package Sliding_Window.B_Medium;
+
+public class _0209_Minimum_Size_Subarray_Sum {
+
+    public int minSubArrayLen(int target, int[] nums) {
+        int sum = 0;
+        int l = 0;
+
+        int ans = Integer.MAX_VALUE;
+        for (int r = 0; r < nums.length; r++) {
+            sum += nums[r];
+            while (sum >= target && sum - nums[l] >= target) {
+                sum -= nums[l++];
+            }
+            if (sum >= target) {
+                ans = Math.min(ans, r - l + 1);
+            }
+        }
+        return ans == Integer.MAX_VALUE ? 0 : ans;
+    }
+}
