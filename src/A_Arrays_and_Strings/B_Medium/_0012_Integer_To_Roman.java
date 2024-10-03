89ce0244f0e25be4564783fd4c7f37c5289c3a91
Nowshad Shaik <nowshad7037@gmail.com>
2024-10-03 16:31:07 +0530

    Add solutions: 12, 496, 504, 2240

---
+++ src/A_Arrays_and_Strings/B_Medium/_0012_Integer_To_Roman.java
@@ -0,0 +1,20 @@
+package A_Arrays_and_Strings.B_Medium;
+
+public class _0012_Integer_To_Roman {
+
+    public String intToRoman(int num) {
+        StringBuilder res = new StringBuilder();
+        int[] v = new int[]{1, 4, 5, 9, 10, 40, 50, 90, 100, 400, 500, 900, 1000};
+        String[] s = new String[]{"I", "IV", "V", "IX", "X", "XL", "L", "XC", "C", "CD", "D", "CM", "M"};
+
+        for (int i = v.length - 1; i >= 0; i--) {
+            int n = num / v[i];
+            for (int j = 0; j < n; j++) {
+                res.append(s[i]);
+            }
+            num = num % v[i];
+        }
+
+        return res.toString();
+    }
+}
