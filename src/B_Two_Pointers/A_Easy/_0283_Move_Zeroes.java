8487df8b6dc8e7fedb8e32ad077b7d24bb758db1
Nowshad Shaik <nowshad7037@gmail.com>
2024-10-14 18:15:13 +0530

    Add solutions: 283, 962, 1456, 1493

---
+++ src/B_Two_Pointers/A_Easy/_0283_Move_Zeroes.java
@@ -0,0 +1,15 @@
+package B_Two_Pointers.A_Easy;
+
+public class _0283_Move_Zeroes {
+
+    public void moveZeroes(int[] nums) {
+        int p1 = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                nums[p1++] = nums[i];
+            }
+        }
+
+        while (p1 < nums.length) nums[p1++] = 0;
+    }
+}
