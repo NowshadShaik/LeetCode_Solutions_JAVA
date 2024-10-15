06a7c7d7a6c106fa02008751e57a8257cc328c6d
Nowshad Shaik <nowshad7037@gmail.com>
2024-10-15 21:50:22 +0530

    Add solutions: 1207, 1657, 2215, 2352

---
+++ src/A_Arrays_and_Strings/A_Easy/_2215_Find_The_Difference_Of_Two_Arrays.java
@@ -0,0 +1,35 @@
+package A_Arrays_and_Strings.A_Easy;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class _2215_Find_The_Difference_Of_Two_Arrays {
+
+    public List<List<Integer>> findDifference(int[] nums1, int[] nums2) {
+        List<List<Integer>> ans = new ArrayList<>();
+        ans.add(new ArrayList<>());
+        ans.add(new ArrayList<>());
+        Set<Integer> set1 = new HashSet<>();
+        Set<Integer> set2 = new HashSet<>();
+        for (int i : nums1) {
+            set1.add(i);
+        }
+        for (int i : nums2) {
+            set2.add(i);
+        }
+        for (int i : set1) {
+            if (!set2.contains(i)) {
+                ans.get(0).add(i);
+            }
+        }
+        for (int i : set2) {
+            if (!set1.contains(i)) {
+                ans.get(1).add(i);
+            }
+        }
+        return ans;
+
+    }
+}
