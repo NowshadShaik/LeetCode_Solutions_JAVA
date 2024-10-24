c3946717efde69f102072bdef43bb96f4187e442
Nowshad Shaik <nowshad7037@gmail.com>
2024-10-24 23:33:12 +0530

    Add solution for 112: Path sum

---
+++ src/J_Binary_Tree/A_Easy/_0112_Path_Sum.java
@@ -0,0 +1,17 @@
+package J_Binary_Tree.A_Easy;
+
+import J_Binary_Tree.TreeNode;
+
+public class _0112_Path_Sum {
+
+    public boolean hasPathSum(TreeNode root, int targetSum) {
+        return traverse(root, 0, targetSum);
+    }
+
+    private boolean traverse(TreeNode node, int sum, int target) {
+        if(node==null) return false;
+        sum += node.val;
+        if(node.left==null && node.right==null && sum == target) return true;
+        return traverse(node.left, sum, target) || traverse(node.right, sum, target);
+    }
+}
