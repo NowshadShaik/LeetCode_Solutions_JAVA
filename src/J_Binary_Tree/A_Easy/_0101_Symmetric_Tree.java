59c082b2aabbc00d50ef3443c3f35a60aa9cbee1
Nowshad Shaik <nowshad7037@gmail.com>
2024-10-26 20:05:55 +0530

    Add solution _0101_Symmetric_Tree

---
+++ src/J_Binary_Tree/A_Easy/_0101_Symmetric_Tree.java
@@ -0,0 +1,17 @@
+package J_Binary_Tree.A_Easy;
+
+import J_Binary_Tree.TreeNode;
+
+public class _0101_Symmetric_Tree {
+
+    public boolean isSymmetric(TreeNode root) {
+        boolean ans = isSame(root.left, root.right);
+        return ans;
+    }
+    private boolean isSame(TreeNode p, TreeNode q){
+        if(p == null || q == null){
+            return p == q;
+        }
+        return (p.val == q.val) && isSame(p.left,q.right) && isSame(p.right,q.left);
+    }
+}
