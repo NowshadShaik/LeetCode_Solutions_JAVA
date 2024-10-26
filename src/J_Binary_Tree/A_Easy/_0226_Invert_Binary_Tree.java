1dc22aecc16e7af5697a42aa9c47f186d3753cbb
Nowshad Shaik <nowshad7037@gmail.com>
2024-10-26 19:54:28 +0530

    Add solution _0226_Invert_Binary_Tree

---
+++ src/J_Binary_Tree/A_Easy/_0226_Invert_Binary_Tree.java
@@ -0,0 +1,22 @@
+package J_Binary_Tree.A_Easy;
+
+import J_Binary_Tree.TreeNode;
+
+public class _0226_Invert_Binary_Tree {
+
+    public TreeNode invertTree(TreeNode root) {
+        invert(root);
+        return root;
+    }
+
+    private void invert(TreeNode node) {
+        if(node == null) return;
+
+        invert(node.left);
+        invert(node.right);
+
+        TreeNode temp = node.left;
+        node.left = node.right;
+        node.right = temp;
+    }
+}
