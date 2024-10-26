5c60cc2695138ae5d665b8e3cdbae32e352f6457
Nowshad Shaik <nowshad7037@gmail.com>
2024-10-26 18:51:06 +0530

    Add solution _0094_Binary_Tree_Inorder_Traversal

---
+++ src/J_Binary_Tree/A_Easy/_0094_Binary_Tree_Inorder_Traversal.java
@@ -0,0 +1,23 @@
+package J_Binary_Tree.A_Easy;
+
+import J_Binary_Tree.TreeNode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class _0094_Binary_Tree_Inorder_Traversal {
+
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        traverse(root, res);
+        return res;
+    }
+
+    private void traverse(TreeNode node, List<Integer> res) {
+        if(node==null) return;
+
+        traverse(node.left, res);
+        res.add(node.val);
+        traverse(node.right, res);
+    }
+}
