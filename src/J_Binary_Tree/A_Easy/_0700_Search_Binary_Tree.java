621461fc12e1ab88b89c3d5b4b2c414779350648
Nowshad Shaik <nowshad7037@gmail.com>
2024-10-29 19:28:47 +0530

    Add solution _0700_Search_Binary_Tree

---
+++ src/J_Binary_Tree/A_Easy/_0700_Search_Binary_Tree.java
@@ -0,0 +1,15 @@
+package J_Binary_Tree.A_Easy;
+
+import J_Binary_Tree.TreeNode;
+
+public class _0700_Search_Binary_Tree {
+
+    class Solution {
+        public TreeNode searchBST(TreeNode root, int val) {
+            if(root==null) return null;
+            if(root.val == val) return root;
+            if(root.val < val) return searchBST(root.right, val);
+            else return searchBST(root.left, val);
+        }
+    }
+}
