a6442feaf74b2b6bc991da2ca459bd67c86a8ef8
Nowshad Shaik <nowshad7037@gmail.com>
2024-11-01 12:10:52 +0530

    Add solution _1957_Delete_Characters_To_Make_Fancy_String

---
+++ src/A_Arrays_and_Strings/A_Easy/_1957_Delete_Characters_To_Make_Fancy_String.java
@@ -0,0 +1,20 @@
+package A_Arrays_and_Strings.A_Easy;
+
+public class _1957_Delete_Characters_To_Make_Fancy_String {
+
+    public String makeFancyString(String s) {
+        if (s.length() <= 2) return s;
+
+        StringBuilder res = new StringBuilder();
+        int consecutive = 1;
+
+        for (int i = 0; i < s.length(); i++) {
+            if (i > 0 && s.charAt(i - 1) == s.charAt(i)) consecutive++;
+            else consecutive = 1;
+
+            if (consecutive < 3) res.append(s.charAt(i));
+        }
+
+        return res.toString();
+    }
+}
