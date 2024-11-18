1aeb044935c63d6a8099aca02a2c4257cbfd61fd
Nowshad Shaik <nowshad7037@gmail.com>
2024-11-18 09:36:49 +0530

    Add solution _1652_Defuse_The_Bomb

---
+++ src/Solutions/_1652_Defuse_The_Bomb.java
@@ -0,0 +1,26 @@
+package Solutions;
+
+public class _1652_Defuse_The_Bomb {
+
+    public int[] decrypt(int[] code, int k) {
+        int n = code.length;
+        int[] res = new int[n];
+
+        if (k == 0) {
+            return res;
+        }
+
+        for (int i = 0; i < n; i++) {
+            if (k > 0) {
+                for (int j = i + 1; j < i + k + 1; j++) {
+                    res[i] += code[j % n];
+                }
+            } else {
+                for (int j = i - Math.abs(k); j < i; j++) {
+                    res[i] += code[(j + n) % n];
+                }
+            }
+        }
+        return res;
+    }
+}
