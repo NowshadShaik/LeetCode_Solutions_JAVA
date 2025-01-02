1dca95d80e38f087b97dff41e656cd705392e4b7
Nowshad Shaik <nowshad7037@gmail.com>
2025-01-02 17:10:14 +0530

    Add solution _0122_Best_Time_To_Buy_And_Sell_Stock_II

---
+++ src/Solutions/_0122_Best_Time_To_Buy_And_Sell_Stock_II.java
@@ -0,0 +1,13 @@
+package Solutions;
+
+public class _0122_Best_Time_To_Buy_And_Sell_Stock_II {
+
+    public int maxProfit(int[] prices) {
+        int res = 0;
+        for (int i = 1; i < prices.length; i++) {
+            if (prices[i] > prices[i - 1])
+                res += prices[i] - prices[i - 1];
+        }
+        return res;
+    }
+}
