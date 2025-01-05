dd3aea719f16a274c6e0eb63505b23c92f59a532
Nowshad Shaik <nowshad7037@gmail.com>
2025-01-05 20:48:02 +0530

    Add solution _0081_Search_In_Rotated_Sorted_Array_II

---
+++ src/Solutions/_0081_Search_In_Rotated_Sorted_Array_II.java
@@ -0,0 +1,31 @@
+package Solutions;
+
+public class _0081_Search_In_Rotated_Sorted_Array_II {
+
+    public boolean search(int[] nums, int target) {
+        int l = 0, r = nums.length - 1;
+
+        while (l <= r) {
+            int m = l + (r - l) / 2;
+
+            if (target == nums[m])
+                return true;
+
+            if (nums[l] < nums[m]) {
+                if (target >= nums[l] && target < nums[m])
+                    r = m - 1;
+                else
+                    l = m + 1;
+            } else if (nums[l] > nums[m]) {
+                if (target > nums[m] && target <= nums[r])
+                    l = m + 1;
+                else
+                    r = m - 1;
+            } else {
+                l++;
+            }
+        }
+
+        return false;
+    }
+}
