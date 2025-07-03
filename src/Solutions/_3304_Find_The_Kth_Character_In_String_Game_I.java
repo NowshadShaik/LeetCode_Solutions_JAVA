b0fa17f247a6e521fdc83359a3ecd2376e6a1801
Nowshad Shaik <nowshad7037@gmail.com>
2025-07-03 21:59:39 +0530

    Add solution _3304_Find_The_Kth_Character_In_String_Game_I

---
+++ src/Solutions/_3304_Find_The_Kth_Character_In_String_Game_I.java
@@ -0,0 +1,21 @@
+package Solutions;
+
+public class _3304_Find_The_Kth_Character_In_String_Game_I {
+
+    public char kthCharacter(int k) {
+        int ans = 0;
+        int t;
+
+        while (k != 1) {
+            t = 31 - Integer.numberOfLeadingZeros(k);
+            if ((1 << t) == k) {
+                t--;
+            }
+
+            k = k - (1 << t);
+            ans++;
+        }
+
+        return (char) ('a' + ans);
+    }
+}
