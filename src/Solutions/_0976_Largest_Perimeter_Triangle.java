1496c4182e254845dd415a0d839ad939c366fac0
Nowshad Shaik <nowshad7037@gmail.com>
2025-10-01 12:00:48 +0530

    Add solution _0976_Largest_Perimeter_Triangle

---
+++ src/Solutions/_0976_Largest_Perimeter_Triangle.java
@@ -0,0 +1,17 @@
+package Solutions;
+
+import java.util.Arrays;
+
+public class _0976_Largest_Perimeter_Triangle {
+
+    public int largestPerimeter(int[] nums) {
+        Arrays.sort(nums);
+
+        for (int i = nums.length - 3; i >= 0; i--) {
+            if (nums[i] + nums[i + 1] > nums[i + 2])
+                return nums[i] + nums[i + 1] + nums[i + 2];
+        }
+
+        return 0;
+    }
+}
